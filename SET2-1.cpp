4a919d72ade7679119479252ccddb88c8d3645f2
Mynthankumar <36624741+Mynthankumar@users.noreply.github.com>
2018-02-28 17:27:58 +0530

    Update SET2-1.cpp

--- SET2-1.cpp
+++ SET2-1.cpp
@@ -7,12 +7,12 @@ int main()
 {
     float base, exponent, result;
  
-    cout << "Enter base and exponent respectively:  ";
+    cout << "Enter base and exponent respectively:\n  ";
     cin >> base >> exponent;
  
     result = pow(base, exponent);
  
-    cout << base << "^" << exponent << " = " << result;
+    cout  << result;
  
     return 0;
 }
