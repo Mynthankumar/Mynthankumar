f94b3345190cf72a9ec03dc66b8f23123d533660
Mynthankumar <36624741+Mynthankumar@users.noreply.github.com>
2018-03-05 12:03:01 +0530

    Update SET3-1.cpp

--- SET3-1.cpp
+++ SET3-1.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-    int n,a,d,i,ans = 0;
-    cout<<"Enter the number of items,starting value and difference"<<endl;
+   int n,a,d,i,ans = 0;
+   cout<<"Enter the number of items,starting value and difference"<<endl;
     cin>>n>>a>>d;
     for(i = 0;i < n;i++){
         ans += a;
-        a += d;
-    }
+        a += d;}
     cout<<ans;
     return 0;
 }
