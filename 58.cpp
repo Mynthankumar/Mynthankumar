17c0bedc2b074095c562845910a60445c84da3aa
Mynthankumar <36624741+Mynthankumar@users.noreply.github.com>
2018-04-02 10:53:52 +0530

    Update 58.cpp

--- 58.cpp
+++ 58.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main()
 {
-  int a,b,temp,i;
-  cout<<"enter thevalue of and b";
-  cin>>a>>b;
-  cout<<"before swapping"<<a<<endl<<b;
-  temp=a;
-  a=b;
-  b=temp;
-  cout<<"after swapping"<<a<<endl<<b;
-  return 0;
+   int a,b,temp,i;
+   cout<<"enter thevalue of and b";
+   cin>>a>>b;
+   cout<<"before swapping"<<a<<endl<<b;
+   temp=a;
+   a=b;
+   b=temp;
+   cout<<"after swapping"<<a<<endl<<b;
+   return 0;
 }
